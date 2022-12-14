afc1b70af241d37c60b5ac6a99e479a1d757fb72
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-15 03:11:22 +0800

    UI fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="sticky inset-x-0 top-0 flex flex-col md:flex-row mx-auto h-screen max-w-[1350px] items-center md:justify-between px-8 mt-4 space-y-8 md:mt-0 md:space-y-0">
+    <section className="sticky inset-x-0 top-0 flex flex-col md:flex-row mx-auto h-screen max-w-[1350px] items-center md:justify-between px-8 pt-16 space-y-8 md:pt-0 md:space-y-0">
       <div className="space-y-4 md:space-y-8">
         <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
           <div className="text-transparent bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text">
