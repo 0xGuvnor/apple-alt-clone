e27ed8b673e96d07ac6267fe10704dcc28998d6d
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-18 16:08:24 +0800

    Footer refinements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="grid grid-cols-1 px-2 py-4 text-sm divide-y divide-gray-400 sm:px-16 md:divide-none md:grid-cols-3">
-      <div className="flex flex-wrap items-center justify-center py-2 divide-x divide-gray-400 md:col-start-2">
+    <div className="grid grid-cols-1 px-2 py-4 text-sm sm:px-16 lg:divide-none lg:grid-cols-3">
+      <div className="flex flex-wrap items-center justify-center py-2 divide-x divide-gray-400 lg:col-span-2 xl:col-span-1 xl:col-start-2">
         <div className="px-2 cursor-pointer hover:underline">
           Privacy Policy
         </div>
@@ -16,7 +16,7 @@ const Footer = () => {
         href="https://github.com/0xGuvnor/apple-alt-clone"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center justify-center py-2 font-mono hover:underline"
+        className="flex items-center justify-center py-2 font-mono hover:underline lg:col-start-3"
       >
         👨‍💻 Made by 0xGuvnor
       </a>
