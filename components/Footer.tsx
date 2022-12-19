549fbb0171f05a8ac809cce4f379b64df6a61c4a
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-19 14:39:27 +0800

    css refactor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,11 @@ const Footer = () => {
   return (
     <div className="grid grid-cols-1 px-2 py-4 text-sm sm:px-16 lg:divide-none lg:grid-cols-3">
       <div className="flex flex-wrap items-center justify-center py-2 divide-x divide-gray-400 lg:col-span-2 xl:col-span-1 xl:col-start-2">
-        <div className="px-2 cursor-pointer hover:underline">
-          Privacy Policy
-        </div>
-        <div className="px-2 cursor-pointer hover:underline">Terms of Use</div>
-        <div className="px-2 cursor-pointer hover:underline">
-          Sales and Refunds
-        </div>
-        <div className="px-2 cursor-pointer hover:underline">Legal</div>
-        <div className="px-2 cursor-pointer hover:underline">Site Map</div>
+        <div className="footerLink">Privacy Policy</div>
+        <div className="footerLink">Terms of Use</div>
+        <div className="footerLink">Sales and Refunds</div>
+        <div className="footerLink">Legal</div>
+        <div className="footerLink">Site Map</div>
       </div>
       <a
         href="https://github.com/0xGuvnor/apple-alt-clone"
