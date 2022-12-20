9d37910769397313f5cd00e0c9d61f4d114442da
0xpaprhnds <101540063+0xpaprhnds@users.noreply.github.com>
2022-12-20 15:31:46 +0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <section className="sticky top-0 flex mx-auto h-screen max-w-[1350px] items-start justify-center px-8 pt-[140px] sm:pt-[68px]">
+    <section className="sticky top-0 flex flex-col mx-auto h-screen max-w-[1350px] items-center justify-between px-8 pt-[140px] sm:pt-[68px]">
       <motion.div
         animate={{ y: 0, scale: 1 }}
         initial={{ y: -300, scale: 0 }}
@@ -12,7 +12,7 @@ const Hero = () => {
         className="z-50 flex flex-col items-center justify-center space-y-4 md:space-y-6"
       >
         <h1 className="flex flex-col items-center justify-center space-y-2 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
-          <div className="text-transparent bg-gradient-to-br from-pink-500 via-purple-500 to-sky-500 bg-clip-text">
+          <div className="font-semibold text-transparent bg-gradient-to-br from-pink-500 via-purple-500 to-sky-500 bg-clip-text">
             iPhone 14
           </div>
           <div className="text-3xl font-medium">Big and bigger.</div>
@@ -27,15 +27,15 @@ const Hero = () => {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 1.5, ease: "easeInOut" }}
-        className="absolute bottom-0 w-full h-[60vh] sm:h-[80vh] lg:h-[78vh] xl:h-[72vh] self-end transition duration-300 overflow-hidden"
+        transition={{ duration: 3, ease: "easeInOut" }}
+        className="absolute bottom-0 inset-x-0 w-full h-[60vh] sm:h-[80vh] xl:h-[74vh] self-end transition duration-300"
       >
         <Image
           src="/iphone14_hero.jpg"
           alt="Hero Shot of iPhone 14"
           quality={100}
           fill
-          className="object-cover"
+          className="object-cover object-top"
         />
       </motion.div>
     </section>
