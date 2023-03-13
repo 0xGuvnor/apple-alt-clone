cb01700edda2ecfeaf4429b6d2db85d93728673b
0xpaprhnds <yokeyeong@me.com>
2023-03-13 18:26:52 +0100

    Using Axios instead of Fetch

--- utils/fetchCategories.ts
+++ utils/fetchCategories.ts
@@ -1,9 +1,16 @@
+import axios from "axios";
+
 export const fetchCategories = async () => {
-  const res = await fetch(
+  const res = await axios.get(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getCategories`
   );
 
-  const data = await res.json();
+  // await fetch(
+  //   `${process.env.NEXT_PUBLIC_BASE_URL}/api/getCategories`
+  // );
+
+  // const data = await res.json();
+  const data = res.data;
   const categories: Category[] = data.categories;
 
   return categories;
